5607fcf73b83427e67a7a23397c3f4c900df452b
Simon Howard <fraggle@gmail.com>
2001-01-14 21:08:31 +0000

    Allegro MIDI structure for other platforms

---
+++ midi.h
@@ -0,0 +1,43 @@
+// Emacs style mode select -*- C++ -*-
+//-------------------------------------------------------------------------
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 
+//  02111-1307, USA.
+//
+// DESCRIPTION:
+//     Allegro MIDI structure: so we can use mmus2mid without
+//     allegro
+//
+//-----------------------------------------------------------------------------
+
+#ifndef __MIDI_H__
+#define __MIDI_H__
+
+// in djgpp version this is declared in allegro.h
+
+#ifndef DJGPP
+
+typedef struct MIDI                    /* a midi file */
+{
+   int divisions;                      /* number of ticks per quarter note */
+   struct {
+      unsigned char *data;             /* MIDI message stream */
+      int len;                         /* length of the track data */
+   } track[MIDI_TRACKS]; 
+} MIDI;
+
+#endif /* #ifndef DJGPP */
+
+#endif
